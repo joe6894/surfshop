6084cf36da1ae0a0c66a2f6e9a080e4b38403fdf
joe6894 <kellyjoseph18@gmail.com>
2020-12-03 15:48:51 +0000

    added background-image, added javascript for select dropdown and added btt link

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -16,7 +18,7 @@ def all_products(request):
 
     if request.GET:
         if 'sort' in request.GET:
-            sortKey = request.GET['sort']
+            sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
@@ -24,11 +26,11 @@ def all_products(request):
             if sortkey == 'category':
                 sortkey = 'category__name'
             if 'direction' in request.GET:
-                direction = rquest.GET['direction']
+                direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -37,7 +39,7 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search critera!")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
