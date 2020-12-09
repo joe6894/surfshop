49c9ae719ababf6414502937f451adddd2825208
joe6894 <kellyjoseph18@gmail.com>
2020-12-09 12:19:07 +0000

    added product sizes to product model and added dropdown size list to product detail

--- products/models.py
+++ products/models.py
@@ -25,6 +25,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     brand = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_big = models.ImageField(null=True, blank=True)
