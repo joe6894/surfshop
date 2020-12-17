e09b1aa4c7a49cb646ac6b170f81e53e53c5945d
joe6894 <kellyjoseph18@gmail.com>
2020-12-17 18:09:18 +0000

    added footer to base.html and equipment-area to index.html

--- home/views.py
+++ home/views.py
@@ -7,9 +7,13 @@ def index(request):
     """ A view to return the index page """
 
     products = Product.objects.filter(category=17)
+    billabong = Product.objects.filter(brand='Billabong')
+
+    print(billabong)
 
     context = {
         'products': products,
+        'billabong': billabong,
     }
 
     return render(request, 'home/index.html', context)
