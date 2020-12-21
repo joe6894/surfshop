22abbaa06de88a447b43795e65bf132468919062
joe6894 <kellyjoseph18@gmail.com>
2020-12-21 11:45:10 +0000

    top-brand links are now working

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('billabong/', views.billabong, name='billabong'),
+    path('animal/', views.animal, name='animal'),
+    path('oneill/', views.oneill, name='oneill'),
+    path('gul/', views.gul, name='gul'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from products.models import Product
 
 # Create your views here.
@@ -7,11 +7,51 @@ def index(request):
     """ A view to return the index page """
 
     products = Product.objects.filter(category=17)
-    billabong = Product.objects.filter(brand='Billabong')
 
     context = {
         'products': products,
-        'billabong': billabong,
     }
 
     return render(request, 'home/index.html', context)
+
+def billabong(request):
+
+    products = Product.objects.filter(brand='Billabong')
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+def animal(request):
+
+    products = Product.objects.filter(brand='Animal')
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+def oneill(request):
+
+    products = Product.objects.filter(brand="O'neill")
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+def gul(request):
+
+    products = Product.objects.filter(brand='Gul')
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
