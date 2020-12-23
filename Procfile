844543486de97d148a00d42cdc52a17f68a4f580
joe6894 <kellyjoseph18@gmail.com>
2020-12-23 16:03:02 +0000

    deploying

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn surfshop.wsgi:application
--- surfshop/settings.py
+++ surfshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'a0x_#n6q*@8s-fpavyie!wtc@650$e5d$-dwnybajpv3u^qh--'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joe6894-surfshop-herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,18 @@ WSGI_APPLICATION = 'surfshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
- }
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
