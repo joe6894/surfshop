dc2547dced34cc0f6cb5d1a527ff5492ebbfac9e
joe6894 <kellyjoseph18@gmail.com>
2020-12-23 16:18:23 +0000

    removed secret key and changed debug

--- surfshop/settings.py
+++ surfshop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a0x_#n6q*@8s-fpavyie!wtc@650$e5d$-dwnybajpv3u^qh--'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['joe6894-surfshop.herokuapp.com', 'localhost']
 
