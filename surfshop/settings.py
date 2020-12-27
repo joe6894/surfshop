9bb02fbf3d805d39038ed4afaeb814d2ad70df7e
joe6894 <kellyjoseph18@gmail.com>
2020-12-27 12:59:48 +0000

    bug fix: internal sever error when registering

--- surfshop/settings.py
+++ surfshop/settings.py
@@ -213,9 +213,9 @@ DEFAULT_FROM_EMAIL = 'surfshop@example.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'surfshop@example.com'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackends'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
